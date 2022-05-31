57df68bbd148414f6dcec3179b32c6a76dc7a8d4
HiJack-M <329304645@qq.com>
2022-05-31 19:20:16 +0800

    feat(webpack): 添加对 react 的解析

--- play-webpack-project/src/search.js
+++ play-webpack-project/src/search.js
@@ -1,1 +1,10 @@
-document.write('search page.');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends React.Component {
+  render() {
+    return <div>Search Page</div>
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById('root'))
